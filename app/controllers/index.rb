181f1529a3405ee83ff723857176893df77d88e6
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 22:03:23 -0500

    finished post create and category views
    routing for each category view with each post

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,29 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/post/create/' do
+  erb :_create_post
+end
+
+get '/categories/:id/' do
+  @category = Category.find(params[:id])
+  erb :categories
+end
+
+get '/posts/:id/' do
+  @post = Post.find(params[:id])
+  erb :post_view
+end
+
+
+############ POSTS ####################
+
+post '/post/create/' do
+  @post_info = Post.new(params[:post])
+  if @post_info.save
+    redirect to ('/')
+  else
+    erb :_create_post
+  end
+end
